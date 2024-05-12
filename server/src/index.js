c04613ccb436a3406da32ba7a4a6644901f0110e
Gauri Pidatala <vp223@cornell.edu>
2024-05-12 01:20:39 -0700

    changing file destination.

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,9 @@ app.post('/entries', async (req, res) => {
     const date = new Date();
     const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
     const formattedTime = `${date.getHours().toString().padStart(2, '0')}-${date.getMinutes().toString().padStart(2, '0')}-${date.getSeconds().toString().padStart(2, '0')}`;
-    const filename = `journal_${formattedDate}_${formattedTime}.md`;
+    console.log(formattedTime, formattedDate);
+    // const filename = `journal_${formattedDate}_${formattedTime}.md`;
+    const filename = `README.md`;
     const content = `# Journal Entry: ${formattedDate} ${formattedTime}\n\n${entry}\n`;
 
     try {
